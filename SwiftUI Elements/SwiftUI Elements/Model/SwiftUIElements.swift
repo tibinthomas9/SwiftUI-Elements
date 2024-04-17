e20832d379816bdaae0839a0f1001038a16fa4ff
Tibin Thomas <tibinmutholy@gmail.com>
2024-04-16 20:43:27 -0400

    day 2

--- SwiftUI Elements/SwiftUI Elements/Model/SwiftUIElements.swift
+++ SwiftUI Elements/SwiftUI Elements/Model/SwiftUIElements.swift
@@ -25,6 +25,8 @@ enum SwiftUIElement {
     enum Section: String, Codable, CaseIterable {
         case userInterface = "User Interface"
         case viewsAndControls = "Views and Controls"
+        case viewStyles = "Views Styles"
+        case animations = "Animations"
         case gestures = "Gestures"
         case layoutViews = "Layout Views"
         case presentation = "Presentation"
@@ -56,6 +58,17 @@ enum SwiftUIElement {
                 return [
                     .spacer, .divider, .padding, .alignmentGuides, .frame
                 ]
+                
+                
+            case .viewStyles:
+                return [
+                    .buttonStyle, .pickerStyle, .menuStyle, .toggleStyle, .gaugeStyle, .progressViewStyle, .labelStyle, .textFieldStyle, .textEditorStyle, .listStyle, .tableStyle, .disclosureGroupStyle, .navigationSplitViewStyle, .tabViewStyle, .controlGroupStyle, .formStyle, .groupBoxStyle, .indexViewStyle, .labeledContentStyle
+
+                ]
+                
+            case .animations:
+                return []
+                
             }
         }
     }
@@ -67,6 +80,7 @@ enum SwiftUIElement {
         case vStack, hStack, zStack, scrollView, navigationView
         case implicitAnimation, explicitAnimation
         case spacer, divider, padding, alignmentGuides, frame
+        case buttonStyle, pickerStyle ,menuStyle, toggleStyle, gaugeStyle, progressViewStyle, labelStyle, textFieldStyle, textEditorStyle, listStyle, tableStyle, disclosureGroupStyle, navigationSplitViewStyle, tabViewStyle, controlGroupStyle, formStyle, groupBoxStyle, indexViewStyle, labeledContentStyle
         
         var description: String {
             switch self {
@@ -97,7 +111,46 @@ enum SwiftUIElement {
             case .padding: return "Adds padding (spacing) around a view or group of views."
             case .alignmentGuides: return "Guides that help align views within their parent container, such as leading, trailing, center, etc."
             case .frame: return "Allows you to set the size and positioning of a view relative to its parent."
+                
+            // View Styles
+            case .buttonStyle: return "Provides customization options for the appearance and behavior of buttons."
+            case .pickerStyle: return "Allows customization of the appearance and behavior of pickers."
+            case .menuStyle: return "Defines the appearance and behavior of menus."
+            case .toggleStyle: return "Customizes the appearance and behavior of toggles."
+                // Indicators
+            case .gaugeStyle: return "Defines the appearance and behavior of gauge-style views."
+            case .progressViewStyle: return "Customizes the appearance and behavior of progress views."
+                // Views that display text
+            case .labelStyle: return "Provides customization options for the appearance and behavior of labels."
+            case .textFieldStyle: return "Allows customization of the appearance and behavior of text fields."
+            case .textEditorStyle: return "Defines the appearance and behavior of text editors."
+                // Collection views
+            case .listStyle: return "Customizes the appearance and behavior of lists."
+            case .tableStyle: return "Provides customization options for the appearance and behavior of tables."
+            case .disclosureGroupStyle: return "Defines the appearance and behavior of disclosure groups."
+                // Navigation views
+            case .navigationSplitViewStyle: return "Customizes the appearance and behavior of navigation split views."
+            case .tabViewStyle: return "Allows customization of the appearance and behavior of tab views."
+                // Groups
+            case .controlGroupStyle: return "Defines the appearance and behavior of control groups."
+            case .formStyle: return "Customizes the appearance and behavior of forms."
+            case .groupBoxStyle: return "Provides customization options for the appearance and behavior of group boxes."
+            case .indexViewStyle: return "Allows customization of the appearance and behavior of index views."
+            case .labeledContentStyle: return "Defines the appearance and behavior of labeled content views."
             }
         }
     }
 }
+
+/*
+ 
+ Tabs:
+  -App Structure
+ - Data and Storage
+ - Views
+ - View Layout
+ - Event Handling
+ - Accessibility
+ - Framework Integration
+ - Tool Support
+ */
